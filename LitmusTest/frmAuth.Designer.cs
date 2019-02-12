87b86f10aaeb71475a7703c905f0b1ffd8ddc79a
ramya <ramyashankaran@yahoo.com>
2019-02-11 23:59:06 -0500

    Initial Commit

---
+++ LitmusTest/frmAuth.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace LitmusApp
+{
+    partial class frmAuth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAuth));
+            this.pbSignIn = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSignIn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbSignIn
+            // 
+            this.pbSignIn.BackgroundImage = global::LitmusTest.Properties.Resources.gsignin;
+            this.pbSignIn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbSignIn.Location = new System.Drawing.Point(99, 230);
+            this.pbSignIn.Name = "pbSignIn";
+            this.pbSignIn.Size = new System.Drawing.Size(248, 66);
+            this.pbSignIn.TabIndex = 4;
+            this.pbSignIn.TabStop = false;
+            this.pbSignIn.Click += new System.EventHandler(this.pbSignIn_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::LitmusTest.Properties.Resources.iconfinder_icon_ios7_plus_outline_211802;
+            this.pictureBox3.Location = new System.Drawing.Point(225, 110);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 32);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::LitmusTest.Properties.Resources.iconfinder_Camera_2431363;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(284, 78);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(107, 98);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(40, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(162, 98);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmAuth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(449, 397);
+            this.Controls.Add(this.pbSignIn);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmAuth";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Litmus App";
+            ((System.ComponentModel.ISupportInitialize)(this.pbSignIn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pbSignIn;
+    }
+}
---
+++ LitmusTest/frmAuth.cs
@@ -0,0 +1,71 @@
+﻿using Auth0.OidcClient;
+using IdentityModel.OidcClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LitmusApp
+{
+    public partial class frmAuth : Form
+    {
+        #region Constructor
+
+        public frmAuth()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Auth0 
+
+        private async void  StartAuthorizationAsync()
+        {
+ 
+            var client = new Auth0Client(new Auth0ClientOptions
+            {
+                Domain = "testytenant.auth0.com",
+                ClientId = "MzBycPYSY5px0yM6MFDcLpX1uVxN0lzA"
+            });
+
+            var extraParameters = new Dictionary<string, string>();
+
+            extraParameters.Add("connection", string.Empty);
+
+            extraParameters.Add("audience", string.Empty);
+
+            InitializeEmailWindow(await client.LoginAsync(extraParameters: extraParameters));
+         
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void InitializeEmailWindow(LoginResult loginResult)
+        {
+            if (!loginResult.IsError)
+            {
+                frmMain emailForm = new frmMain(loginResult.User.Identity.Name);
+                emailForm.ShowDialog();
+            }
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void pbSignIn_Click(object sender, EventArgs e)
+        {
+            StartAuthorizationAsync();
+        }
+
+        #endregion
+    }
+}
